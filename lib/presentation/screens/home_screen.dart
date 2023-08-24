c8d665068ce3aac5e9c8154bdf5e1221fc1a98b3
Hernan Pormachi <encora-hernan.pormachi@mibanco.com.pe>
2023-08-24 12:55:29 -0500

    usando item builder

--- lib/presentation/screens/home_screen.dart
+++ lib/presentation/screens/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:widgets_app/config/menu/menu_items.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -9,6 +10,28 @@ class HomeScreen extends StatelessWidget {
       appBar: AppBar(
         title: Text("Flutter + Material 3"),
       ),
+      body: _HomeView(),
+    );
+  }
+}
+
+class _HomeView extends StatelessWidget {
+  const _HomeView({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemCount: appMenuItems.length,
+      itemBuilder: (context, index) {
+        final item = appMenuItems[index];
+
+        return ListTile(
+          title: Text(item.title),
+          subtitle: Text(item.subTitle),
+        );
+      },
     );
   }
 }
