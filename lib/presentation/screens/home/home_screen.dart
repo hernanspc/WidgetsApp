e2f0b0d59ea0919082ace5790dc216ad1b192acb
Hernan Pormachi <encora-hernan.pormachi@mibanco.com.pe>
2023-08-27 16:42:28 -0500

    rutas con nombre implementado

--- lib/presentation/screens/home/home_screen.dart
+++ lib/presentation/screens/home/home_screen.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:widgets_app/config/menu/menu_items.dart';
+import 'package:widgets_app/presentation/screens/cards/cards_screen.dart';
 
 class HomeScreen extends StatelessWidget {
+  static const name = 'home_screen';
   const HomeScreen({super.key});
 
   @override
@@ -65,7 +67,10 @@ class _CustomListTitle extends StatelessWidget {
         // Navigator.of(context).pushNamed(menuItem.link);
         // Navigator.popAndPushNamed(context, menuItem.link);
         // Navigator.pushNamed(context, menuItem.link);
-        context.push(menuItem.link);
+        // context.push(
+        //   menuItem.link,
+        // );
+        context.pushNamed(CardsScreen.name);
       },
     );
   }
