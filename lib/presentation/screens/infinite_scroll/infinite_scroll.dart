5537e4e03438d6e3677e1e75dcf840048120c3dd
Hernan Pormachi <encora-hernan.pormachi@mibanco.com.pe>
2023-08-28 18:53:18 -0500

    infinite

--- lib/presentation/screens/infinite_scroll/infinite_scroll.dart
+++ lib/presentation/screens/infinite_scroll/infinite_scroll.dart
@@ -14,18 +14,24 @@ class _InfiniteScrollScreenState extends State<InfiniteScrollScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: ListView.builder(
-        itemCount: imageIds.length,
-        itemBuilder: (context, index) {
-          return FadeInImage(
-            fit: BoxFit.cover,
-            width: double.infinity,
-            height: 300,
-            placeholder: const AssetImage('assets/images/jar-loading.gif'),
-            image: NetworkImage(
-                'https://picsum.photos/id/${imageIds[index]}/500/300'),
-          );
-        },
+      backgroundColor: Colors.black,
+      body: MediaQuery.removePadding(
+        context: context,
+        removeTop: true,
+        removeBottom: true,
+        child: ListView.builder(
+          itemCount: imageIds.length,
+          itemBuilder: (context, index) {
+            return FadeInImage(
+              fit: BoxFit.cover,
+              width: double.infinity,
+              height: 300,
+              placeholder: const AssetImage('assets/images/jar-loading.gif'),
+              image: NetworkImage(
+                  'https://picsum.photos/id/${imageIds[index]}/500/300'),
+            );
+          },
+        ),
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {},
