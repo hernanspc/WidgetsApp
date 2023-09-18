b950a7ae1948f234eee9f2e2763df50fbd7730b9
Hernan Pormachi <encora-hernan.pormachi@mibanco.com.pe>
2023-09-18 15:27:37 -0500

    uso de riverpod

--- lib/presentation/screens/counter/counter_screen.dart
+++ lib/presentation/screens/counter/counter_screen.dart
@@ -1,21 +1,30 @@
 import 'package:flutter/material.dart';
+import 'package:ionicons/ionicons.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:widgets_app/presentation/providers/counter_provider.dart';
 
-class CounterScreen extends StatelessWidget {
+class CounterScreen extends ConsumerWidget {
   static const name = 'counter_screen';
   const CounterScreen({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final int clickCounter = ref.watch(counterProvider);
     return Scaffold(
       appBar: AppBar(
         title: const Text('Counter Screen'),
       ),
       body: Center(
-        child: Text('Valor: 10', style: Theme.of(context).textTheme.titleLarge),
+        child: Text('Valor: $clickCounter',
+            style: Theme.of(context).textTheme.titleLarge),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: () {},
+        onPressed: () {
+          // ref.read(counterProvider.notifier).state++;
+          ref.read(counterProvider.notifier).update((state) => state + 1);
+        },
         child: Icon(Icons.add),
+        // child: Icon(Ionicons.add),
       ),
     );
   }
